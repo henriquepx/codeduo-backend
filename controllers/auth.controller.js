504efc6698ecd1f5c7b6d4b7c8fa0ab3b6dcdee1
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-09 14:57:47 -0300

    feat: add middleware to errors

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,14 @@
 import User from "../models/User.model.js";
 import bcrypt from "bcryptjs";
 
-export const Signup = async (req, res) => {
+export const Signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save();
+        res.status(201).json({ msg: "User created successfully" });
     } catch (error) {
-        res.status(500).json({ msg: error.message });
+        next(error);
     }
-    await newUser.save();
-    res.status(201).json({ msg: "User created successfully" });
 }
--- index.js
+++ index.js
@@ -24,3 +24,14 @@ app.listen(PORT, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500; 
+  const message = error.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false, 
+    message,
+    statusCode,
+  });
+})
