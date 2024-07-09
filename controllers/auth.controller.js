4d00ab9b694937cd252e3c5bb14034d24559482d
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-09 15:58:19 -0300

    feat: auth register

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,21 @@
-import User from "../models/User.model.js";
-import bcrypt from "bcryptjs";
+import bcrypt from 'bcryptjs';
+import User from '../models/user.model.js';
 
-export const Signup = async (req, res, next) => {
+export const Signup = async (req, res) => {
+  try {
     const { username, email, password } = req.body;
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    try {
-        await newUser.save();
-        res.status(201).json({ msg: "User created successfully" });
-    } catch (error) {
-        next(error);
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: 'Todos os campos são obrigatórios' });
     }
-}
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'Usuário criado com sucesso' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro no servidor' });
+  }
+};
