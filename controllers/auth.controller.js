420735119522ffd9f890f102a6a155d788c49156
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-09 16:26:13 -0300

    feat: add cookies and complete auth sign in

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcrypt from 'bcryptjs';
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const Signup = async (req, res) => {
   try {
@@ -15,6 +17,22 @@ export const Signup = async (req, res) => {
     await newUser.save();
 
     res.status(201).json({ message: 'Usuário criado com sucesso' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro no servidor' });
+  }
+};
+
+export const Signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'Usuário não encontrado'));
+
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Senha inválida'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    res.cookie('acess_token', token, { httpOnly: true, }).status(200).json(rest);
   } catch (error) {
     res.status(500).json({ message: 'Erro no servidor' });
   }
