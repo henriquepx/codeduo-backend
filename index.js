693dd27d48c94e2d23f85e838d7c72eab2011ba5
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-09 21:51:18 -0300

    feat: cors and adjust uri from bd

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import connectDB from './db/db.js';
 
-dotenv.config();
+dotenv.config({ path: './.env' });
+
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+console.log('PORT:', process.env.PORT);
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
+connectDB();
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
