137800758494caab6c55a43eaa7a44e3f38aebb0
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-16 16:43:07 -0300

    feat: add compression to optimize the content

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import compression from 'compression';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -17,6 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(compression());
 
 const corsOptions = {
   origin: 'https://codeduo.vercel.app',
