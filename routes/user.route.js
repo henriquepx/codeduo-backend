aa1e930fa0fac2f4d7fc5d1ce61adee4868f5b88
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-19 10:01:01 -0300

    feat: add new routes to update/delete

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { createUser } from "../controllers/user.controller.js";
+import { verifyToken } from '../utils/verifyUser.js';
+import { updateUser, deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", createUser);
+router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
