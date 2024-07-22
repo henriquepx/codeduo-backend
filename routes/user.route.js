85bb24489590416797ee5af114038f906384c044
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-22 10:58:21 -0300

    update: update router

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,7 @@ import { updateUser, deleteUser } from "../controllers/user.controller.js";
 const router = express.Router();
 
 router.get("/", createUser);
-router.post("/update/:id", verifyToken, updateUser);
+router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
