7a12fa370e0ecc84d3c1bc275b6833a7f8efdd91
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-23 14:03:11 -0300

    feat: trying to fix cookie error

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -28,7 +28,11 @@ export const Signin = async (req, res, next) => {
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
     const expiryDate = new Date(Date.now() + 3600000);
-    res.cookie('acess_token', token, { httpOnly: true, }).status(200).json(rest);
+    res.cookie('acess_token', token, {
+      httpOnly: true,
+      sameSite: 'None', 
+      secure: true,
+    }).status(200).json(rest);
   } catch (error) {
     next(error);
   }
