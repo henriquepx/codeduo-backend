0023acbe4b0914eb203cc6366fb493f9f551a6b3
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-23 21:03:30 -0300

    -

--- utils/error.js
+++ utils/error.js
@@ -1,6 +1,8 @@
-export default errorHandler = (statusCode, message) => {
+const errorHandler = (statusCode, message) => {
     const error = new Error();
     error.statusCode = statusCode;
     error.message = message;
     return error;
-}
+};
+
+export default errorHandler;
